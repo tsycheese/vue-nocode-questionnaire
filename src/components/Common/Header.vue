896f3de1c5a047ab8315db3e45bfdef247c8304e
梗小姐最好了 <1787129710@qq.com>
2025-05-08 20:54:58 +0800

    完成 Header 组件样式，并添加了返回功能

---
+++ src/components/Common/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="header">
+    <div class="left">
+      <el-button circle size="small" :icon="ArrowLeft" @click="router.go(-1)" />
+    </div>
+    <div class="center"></div>
+    <div class="right">
+      <img :src="imgUrl" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ArrowLeft } from '@element-plus/icons-vue';
+import { useRouter } from 'vue-router';
+import { ref } from 'vue';
+
+const router = useRouter();
+const imgUrl = ref('https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif');
+</script>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  justify-content: space-between;
+  height: 50px;
+  border-bottom: 1px solid var(--border-color);
+}
+
+.left {
+  height: 100%;
+  width: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-right: 1px solid var(--border-color);
+}
+
+.right {
+  height: 100%;
+  width: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-left: 1px solid var(--border-color);
+
+  img {
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+  }
+}
+</style>
