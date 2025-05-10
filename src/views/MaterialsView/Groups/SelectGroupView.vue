72850c5abd898682a9768088b80e8f697b9ff7c5
梗小姐最好了 <1787129710@qq.com>
2025-05-10 14:06:46 +0800

    增添 router-link 的激活样式 link-item-active

--- src/views/MaterialsView/Groups/SelectGroupView.vue
+++ src/views/MaterialsView/Groups/SelectGroupView.vue
@@ -3,11 +3,21 @@
     <!-- 左侧 -->
     <!-- 单选题 | 多选题 | 下拉选择 | 图片单选题 | 图片多选题 -->
     <template #default>
-      <RouterLink class="link-item mb-15" to="/single-select"> 单选题 </RouterLink>
-      <RouterLink class="link-item mb-15" to="/multi-select"> 多选题 </RouterLink>
-      <RouterLink class="link-item mb-15" to="/option-select"> 下拉选择 </RouterLink>
-      <RouterLink class="link-item mb-15" to="/single-pic-select"> 图片单选题 </RouterLink>
-      <RouterLink class="link-item mb-15" to="/multi-pic-select"> 图片多选题 </RouterLink>
+      <RouterLink class="link-item mb-15" active-class="link-item-active" to="/single-select">
+        单选题
+      </RouterLink>
+      <RouterLink class="link-item mb-15" active-class="link-item-active" to="/multi-select">
+        多选题
+      </RouterLink>
+      <RouterLink class="link-item mb-15" active-class="link-item-active" to="/option-select">
+        下拉选择
+      </RouterLink>
+      <RouterLink class="link-item mb-15" active-class="link-item-active" to="/single-pic-select">
+        图片单选题
+      </RouterLink>
+      <RouterLink class="link-item mb-15" active-class="link-item-active" to="/multi-pic-select">
+        图片多选题
+      </RouterLink>
     </template>
   </Layout>
 </template>
