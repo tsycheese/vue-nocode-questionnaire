9548cefca3b65d2e3692a21fbe07fdd15c1ac400
梗小姐最好了 <1787129710@qq.com>
2025-05-10 22:32:24 +0800

    结合 materialStore 和 ts 约束，将 业务组件 SingleSelect 及其用到的组件 的样式与数据仓库的转台绑定

--- src/components/ServeComs/Materials/Common/MaterialHeader.vue
+++ src/components/ServeComs/Materials/Common/MaterialHeader.vue
@@ -1,16 +1,82 @@
 <template>
   <div class="material-header">
-    <h2 class="title">
-      <span>1.</span>
-      <span>Watching the moon</span>
+    <h2
+      class="title"
+      :class="{ 'font-bold': !titleWeight, 'font-italic': !titleItalic }"
+      :style="{ fontSize: `${titleSize}px`, color: titleColor }"
+    >
+      <span>{{ serialNum }}.</span>
+      <span>{{ title }}</span>
     </h2>
-    <div class="desc">我明白</div>
+    <div
+      class="desc"
+      :class="{ 'font-bold': !descWeight, 'font-italic': !descItalic }"
+      :style="{ fontSize: `${descSize}px`, color: descColor }"
+    >
+      {{ desc }}
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps({
+  serialNum: {
+    type: Number,
+    default: 1,
+  },
+  title: {
+    type: String,
+    default: 'Watching the moon',
+  },
+  desc: {
+    type: String,
+    default: '我明白',
+  },
+  titleSize: {
+    type: String,
+    default: '20',
+  },
+  descSize: {
+    type: String,
+    default: '14',
+  },
+  titleWeight: {
+    // 文字粗细
+    // 0 加粗
+    // 1 正常
+    type: Number,
+    default: 0,
+  },
+  descWeight: {
+    type: Number,
+    default: 1,
+  },
+  titleItalic: {
+    // 文字是否倾斜
+    // 0 倾斜
+    // 1 正常
+    type: Number,
+    default: 0,
+  },
+  descItalic: {
+    type: Number,
+    default: 0,
+  },
+  titleColor: {
+    type: String,
+    default: '#303133',
+  },
+  descColor: {
+    type: String,
+    default: '#909399',
+  },
+});
+
+console.log(props);
+</script>
 
 <style scoped lang="scss">
+// 以下样式有被行内样式覆盖的可能（根据数据仓库的特定字段的值而决定）
 h2 {
   font-size: 22px;
   font-weight: normal;
--- src/components/ServeComs/Materials/SelectComs/SingleSelect.vue
+++ src/components/ServeComs/Materials/SelectComs/SingleSelect.vue
@@ -1,19 +1,49 @@
 <template>
-  <div class="single-select">
-    <MaterialHeader />
+  <div class="single-select" :class="{ 'text-center': !!computedStatus.position }">
+    <MaterialHeader
+      :serialNum="serialNum"
+      :title="computedStatus.title"
+      :desc="computedStatus.desc"
+      :titleSize="computedStatus.titleSize"
+      :descSize="computedStatus.descSize"
+      :titleWeight="computedStatus.titleWeight"
+      :descWeight="computedStatus.descWeight"
+      :titleItalic="computedStatus.titleItalic"
+      :descItalic="computedStatus.descItalic"
+      :titleColor="computedStatus.titleColor"
+      :descColor="computedStatus.descColor"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
 import MaterialHeader from '../Common/MaterialHeader.vue';
+import type { OptionsStatus } from '@/types/editProps.ts';
+import {
+  getTextStatus,
+  getStringArrStatus,
+  getCurrentStatus,
+  getStringStatusByCurrentStatus,
+} from '@/utils/index.ts';
 
-const props = defineProps({
-  status: {
-    type: Object,
-  },
-  serialNum: {
-    type: Number,
-  },
-});
-console.log(props);
+const props = defineProps<{
+  status: OptionsStatus;
+  serialNum: number;
+}>();
+
+const computedStatus = computed(() => ({
+  title: getTextStatus(props.status.title),
+  desc: getTextStatus(props.status.desc),
+  options: getStringArrStatus(props.status.options),
+  position: getCurrentStatus(props.status.position), // 通过下标来判断 position 的状态（左对齐：0，居中：1）
+  titleSize: getStringStatusByCurrentStatus(props.status.titleSize),
+  descSize: getStringStatusByCurrentStatus(props.status.descSize),
+  titleWeight: getCurrentStatus(props.status.titleWeight), // 下标判断 weight 状态（加粗：0，正常：1）
+  descWeight: getCurrentStatus(props.status.descWeight),
+  titleItalic: getCurrentStatus(props.status.titleItalic), // 通过下标来判断 titleItalic 的状态（倾斜：0，正常：1）
+  descItalic: getCurrentStatus(props.status.descItalic),
+  titleColor: getTextStatus(props.status.titleColor),
+  descColor: getTextStatus(props.status.descColor),
+}));
 </script>
