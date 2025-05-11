dbbc4ed29b1f4bc4674774d49dd205b6c5603128
梗小姐最好了 <1787129710@qq.com>
2025-05-11 23:59:24 +0800

    materialStore 添加了 addOptionStatus 和 removeOptionStatus 方法，用于操作 status 类型为数组的数据。目前只能操作字符串数组

--- src/stores/actions.ts
+++ src/stores/actions.ts
@@ -1,5 +1,17 @@
-import type { TextProps } from '@/types/editProps.ts';
+import { isStringArr, type OptionsProps, type TextProps } from '@/types/editProps.ts';
 
 export function setTextStatus(textProps: TextProps, text: string) {
   textProps.status = text;
+}
+
+export function addOptionStatus(optionsProps: OptionsProps) {
+  if (isStringArr(optionsProps.status)) {
+    optionsProps.status.push('新选项');
+  }
+}
+
+export function removeOptionStatus(optionsProps: OptionsProps, index: number) {
+  if (isStringArr(optionsProps.status)) {
+    optionsProps.status.splice(index, 1);
+  }
 }
--- src/stores/materialStore.ts
+++ src/stores/materialStore.ts
@@ -1,7 +1,7 @@
 // 组件市场里面所有组件状态的仓库
 import { defineStore } from 'pinia';
 import { defaultStatusMap } from '@/configs/defaultStatus/defaultStatusMap';
-import { setTextStatus } from './actions.ts';
+import { addOptionStatus, removeOptionStatus, setTextStatus } from './actions.ts';
 
 export const useMaterialStore = defineStore('materialStore', {
   state: () => ({
@@ -14,5 +14,7 @@ export const useMaterialStore = defineStore('materialStore', {
   }),
   actions: {
     setTextStatus,
+    addOptionStatus,
+    removeOptionStatus,
   },
 });
