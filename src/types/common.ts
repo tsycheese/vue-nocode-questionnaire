adf3fb41e989a63ca990d7c9fc3d74d1f382a957
梗小姐最好了 <1787129710@qq.com>
2025-05-11 19:22:50 +0800

    新增业务组件状态类型 ComStatus

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,4 +1,15 @@
 import type { defineComponent } from 'vue';
+import type { OptionsProps, TextProps } from './editProps.ts';
 
 // 导出 vue 组件类型
 export type VueComType = ReturnType<typeof defineComponent>;
+
+// 导出业务组件状态类型
+export interface ComStatus {
+  type: VueComType;
+  name: string;
+  id: string;
+  status: {
+    [key: string]: TextProps | OptionsProps;
+  };
+}
