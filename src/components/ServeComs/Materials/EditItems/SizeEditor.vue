13782c8c5ccc5aa14a9423fba9f46ae8c6be1f11
梗小姐最好了 <1787129710@qq.com>
2025-05-12 20:03:04 +0800

    更换按钮组

--- src/components/ServeComs/Materials/EditItems/SizeEditor.vue
+++ src/components/ServeComs/Materials/EditItems/SizeEditor.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="size-editor">
     <EditorButtonGroup :title="curTitle" :status="status[currentStatus]">
-      <div class="options">
-        <div class="option" :class="{ selected: currentStatus === 0 }" @click="curOption = 0">
+      <el-radio-group v-model="curOption" :style="{ height: '100%' }">
+        <el-radio-button :value="0">
           <font-awesome-icon :icon="['fas', 'font']" />
-        </div>
-        <div class="option" :class="{ selected: currentStatus === 1 }" @click="curOption = 1">
+        </el-radio-button>
+        <el-radio-button :value="1">
           <font-awesome-icon :icon="['fas', 'font']" size="sm" />
-        </div>
-        <div class="option" :class="{ selected: currentStatus === 2 }" @click="curOption = 2">
+        </el-radio-button>
+        <el-radio-button :value="2">
           <font-awesome-icon :icon="['fas', 'font']" size="xs" />
-        </div>
-      </div>
+        </el-radio-button>
+      </el-radio-group>
     </EditorButtonGroup>
   </div>
 </template>
@@ -46,37 +46,3 @@ watch(curOption, (newVal) => {
   updateStatus && updateStatus(props.configKey, newVal);
 });
 </script>
-
-<style scoped lang="scss">
-.options {
-  display: flex;
-  border: 1px solid var(--el-border-color);
-  border-radius: 4px;
-}
-
-.option {
-  box-sizing: border-box;
-  padding: 0 15px;
-  height: 32px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-right: 1px solid var(--el-border-color);
-  cursor: pointer;
-  transition: 0.3s;
-
-  &:last-child {
-    border-right: none;
-  }
-
-  &:hover {
-    color: var(--el-color-primary);
-    background-color: var(--el-color-primary-light-9);
-  }
-
-  &.selected {
-    color: var(--el-color-white);
-    background-color: var(--el-color-primary);
-  }
-}
-</style>
