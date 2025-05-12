9692d18da7ae5d0297b13a2887a4c28dbe9066cf
梗小姐最好了 <1787129710@qq.com>
2025-05-12 14:58:19 +0800

    注册 font-awsome-icon 图标组件

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,11 @@ import zhCn from 'element-plus/es/locale/lang/zh-cn';
 // 引入样式
 import './assets/css/index.scss';
 
+// 引入 Font Awesome 图标库
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 const app = createApp(App);
 
 app.use(createPinia());
@@ -20,5 +25,10 @@ app.use(router);
 app.use(ElementPlus, {
   locale: zhCn,
 });
+
+// 将所有图标添加到库中
+library.add(fas);
+// 注册一个 font-awesome-icon 组件，用于在模板中使用图标
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount('#app');
