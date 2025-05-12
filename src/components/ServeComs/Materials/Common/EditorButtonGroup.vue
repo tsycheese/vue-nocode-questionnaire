8e5549d633470357f47c989eda2f6b92334a8cc5
梗小姐最好了 <1787129710@qq.com>
2025-05-12 15:09:52 +0800

    添加 EditorButtonGroup 组件，作为居中、文字尺寸、文字加粗、文字倾斜、文字颜色等编辑组件的公共模板

---
+++ src/components/ServeComs/Materials/Common/EditorButtonGroup.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- 编辑按钮组组件 -->
+  <div class="editor-button-group flex align-items-center space-between">
+    <!-- 标题以及当前状态 -->
+    <div class="flex align-items-center">
+      <div class="mr-20">{{ title }}</div>
+      <div class="currentStatus">{{ status }}</div>
+    </div>
+    <!-- 按钮组 -->
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+  status: {
+    type: String,
+    default: '',
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.currentStatus {
+  color: var(--info-color);
+  font-size: var(--font-size-base);
+}
+</style>
