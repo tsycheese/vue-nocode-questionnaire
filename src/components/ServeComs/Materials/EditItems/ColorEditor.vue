f4073dc32f36b913222a25958f8dd12828f34de1
梗小姐最好了 <1787129710@qq.com>
2025-05-13 12:23:58 +0800

    修改 ColorEditor 的标题

--- src/components/ServeComs/Materials/EditItems/ColorEditor.vue
+++ src/components/ServeComs/Materials/EditItems/ColorEditor.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="italic-editor">
-    <EditorButtonGroup title="居中设置" :status="status">
+    <EditorButtonGroup :title="curTitle" :status="status">
       <el-color-picker v-model="curColor" />
     </EditorButtonGroup>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, inject, watch } from 'vue';
+import { ref, computed, inject, watch } from 'vue';
 import EditorButtonGroup from '../Common/EditorButtonGroup.vue';
 
 const props = defineProps<{
@@ -17,6 +17,13 @@ const props = defineProps<{
   status: string;
   configKey: string;
 }>();
+const curTitle = computed(() => {
+  if (props.configKey === 'titleColor') {
+    return '标题颜色';
+  } else if (props.configKey === 'descColor') {
+    return '描述颜色';
+  }
+});
 const curColor = ref(props.status);
 
 const updateStatus =
