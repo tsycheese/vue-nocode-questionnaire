f497eeea66b4e2da84a0c53a7cf15ced39d0fa42
梗小姐最好了 <1787129710@qq.com>
2025-05-17 18:05:26 +0800

    添加 TextNote 编辑组件的默认状态数据，并添加到 materialStore

---
+++ src/configs/defaultStatus/TextNote.ts
@@ -0,0 +1,124 @@
+// 配置单选题 SingleSelect 和编辑组件的映射关系（EditorStateSchema）
+// 单选题（业务组件） ---> 编辑组件有哪些
+
+import TextNote from '@/components/ServeComs/Materials/NoteComs/TextNote.vue';
+
+import TitleEditor from '@/components/ServeComs/Materials/EditItems/TitleEditor.vue';
+import DescEditor from '@/components/ServeComs/Materials/EditItems/DescEditor.vue';
+import TextTypeEditor from '@/components/ServeComs/Materials/EditItems/TextTypeEditor.vue';
+import PositionEditor from '@/components/ServeComs/Materials/EditItems/PositionEditor.vue';
+import SizeEditor from '@/components/ServeComs/Materials/EditItems/SizeEditor.vue';
+import WeightEditor from '@/components/ServeComs/Materials/EditItems/WeightEditor.vue';
+import ItalicEditor from '@/components/ServeComs/Materials/EditItems/ItalicEditor.vue';
+import ColorEditor from '@/components/ServeComs/Materials/EditItems/ColorEditor.vue';
+import { markRaw } from 'vue';
+import { v4 as uuidv4 } from 'uuid';
+import type { ComStatus } from '@/types/common.ts';
+
+export default function () {
+  return {
+    type: markRaw(TextNote),
+    name: 'text-note',
+    id: uuidv4(),
+    // 组件的状态：组件的每一个能够修改的状态都应该对应一个编辑组件；
+    //            状态 --> 编辑组件（状态和编辑组件是一一对应的）
+    status: {
+      // 多一个type编辑项
+      type: {
+        id: uuidv4(),
+        status: ['标题', '描述'],
+        currentStatus: 1,
+        isShow: true,
+        name: 'text-type-editor',
+        editCom: markRaw(TextTypeEditor),
+      },
+
+      title: {
+        id: uuidv4(),
+        status: '单选题默认标题',
+        isShow: true,
+        name: 'title-editor',
+        editCom: markRaw(TitleEditor),
+      },
+      desc: {
+        id: uuidv4(),
+        status: '单选题默认描述',
+        isShow: true,
+        name: 'desc-editor',
+        editCom: markRaw(DescEditor),
+      },
+
+      // 对齐方式
+      position: {
+        id: uuidv4(),
+        status: ['左对齐', '居中对齐'],
+        currentStatus: 0,
+        isShow: true,
+        name: 'position-editor',
+        editCom: markRaw(PositionEditor),
+      },
+      titleSize: {
+        id: uuidv4(),
+        status: ['22', '20', '18'],
+        currentStatus: 0,
+        isShow: true,
+        name: 'size-editor',
+        editCom: markRaw(SizeEditor),
+      },
+      descSize: {
+        id: uuidv4(),
+        status: ['16', '14', '12'],
+        currentStatus: 0,
+        isShow: true,
+        name: 'size-editor',
+        editCom: markRaw(SizeEditor),
+      },
+      titleWeight: {
+        id: uuidv4(),
+        status: ['加粗', '正常'],
+        currentStatus: 1,
+        isShow: true,
+        name: 'weight-editor',
+        editCom: markRaw(WeightEditor),
+      },
+      descWeight: {
+        id: uuidv4(),
+        status: ['加粗', '正常'],
+        currentStatus: 1,
+        isShow: true,
+        name: 'weight-editor',
+        editCom: markRaw(WeightEditor),
+      },
+      titleItalic: {
+        id: uuidv4(),
+        status: ['斜体', '正常'],
+        currentStatus: 1,
+        isShow: true,
+        name: 'italic-editor',
+        editCom: markRaw(ItalicEditor),
+      },
+      descItalic: {
+        id: uuidv4(),
+        status: ['斜体', '正常'],
+        currentStatus: 1,
+        isShow: true,
+        name: 'italic-editor',
+        editCom: markRaw(ItalicEditor),
+      },
+      titleColor: {
+        id: uuidv4(),
+        status: '#000',
+        isShow: true,
+        name: 'color-editor',
+        editCom: markRaw(ColorEditor),
+      },
+      descColor: {
+        id: uuidv4(),
+        status: '#909399',
+        isShow: true,
+        name: 'color-editor',
+        editCom: markRaw(ColorEditor),
+      },
+    },
+  } as ComStatus;
+}
--- src/configs/defaultStatus/defaultStatusMap.ts
+++ src/configs/defaultStatus/defaultStatusMap.ts
@@ -1,8 +1,10 @@
 // 该文件用于定义默认状态的映射表
 import singleSelectDefaultStatus from './SingleSelect';
 import singlePicSelectDefaultStatus from './SinglePicSelect';
+import textNoteDefaultStatus from './TextNote';
 
 export const defaultStatusMap = {
   'single-select': singleSelectDefaultStatus,
   'single-pic-select': singlePicSelectDefaultStatus,
+  'text-note': textNoteDefaultStatus,
 };
