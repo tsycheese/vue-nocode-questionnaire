b9f098674837c35327eedea25bd2b17a7c5b00b5
梗小姐最好了 <1787129710@qq.com>
2025-05-20 19:06:12 +0800

    完成问卷编辑其中的 Center 页面

--- src/views/EditorView/Center/index.vue
+++ src/views/EditorView/Center/index.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>中间区域</div>
+  <div class="center-container">
+    <div class="serve-com-item" v-for="com in editorStore.coms" :key="com.id">
+      <component :is="com.type" :status="com.status" :serialNum="1"></component>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useEditorStore } from '@/stores/editorStore.ts';
+
+const editorStore = useEditorStore();
+</script>
+
+<style scoped lang="scss">
+.center-container {
+  padding: 20px;
+  overflow-y: auto;
+}
+</style>
