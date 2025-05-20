e6ec407d6d7be95ea82d54550f0bc297ae58ed6e
梗小姐最好了 <1787129710@qq.com>
2025-05-20 19:05:19 +0800

    添加数据状态仓库 editorStore，用于向编辑问卷页面提供数据交互

---
+++ src/stores/editorStore.ts
@@ -0,0 +1,48 @@
+import type { ComStatus } from '@/types/common.ts';
+import { defineStore } from 'pinia';
+import {
+  addOptionStatus,
+  removeOptionStatus,
+  setOptionsStatus,
+  setPicLinkByIndex,
+  setTextStatus,
+} from './actions.ts';
+import type { TypeStatus } from '@/types/editProps.ts';
+import { isServeComName } from '@/types/store.ts';
+
+export const useEditorStore = defineStore('editorStore', {
+  state: () => ({
+    currentComIndex: -1, // 当前选中的组件的索引，-1 表示没有选中任何组件
+    serveCount: 0, // 当前服务组件的数量
+    coms: [] as ComStatus[],
+  }),
+  actions: {
+    /**
+     *  设置说明文字状态，['标题', '段落']
+     */
+    changeTextType(index: number) {
+      const serveCom = this.coms[this.currentComIndex];
+      const status = this.coms[this.currentComIndex].status as TypeStatus;
+      if (serveCom.name === 'text-note' && status['type'].currentStatus !== index) {
+        status['type'].currentStatus = index;
+        // 改变标题和段落的显示状态
+        for (const key in status) {
+          if (key.startsWith('title') || key.startsWith('desc')) {
+            // @ts-ignore
+            status[key].isShow = !status[key].isShow;
+          }
+        }
+      }
+    },
+    addCom(newCom: ComStatus) {
+      this.coms.push(newCom);
+      this.currentComIndex = -1;
+      if (isServeComName(newCom.name)) this.serveCount++;
+    },
+    setTextStatus,
+    addOptionStatus,
+    removeOptionStatus,
+    setOptionsStatus,
+    setPicLinkByIndex,
+  },
+});
